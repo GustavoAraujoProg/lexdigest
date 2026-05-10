ae6a97d1eff2a30fb032646b32996f0bb4f97625
AntonioNevesDS <antonions866@gmail.com>
2026-05-10 13:42:00 -0300

    Telas de perfil e tela main

---
+++ app/src/main/java/com/example/sprint_3_lexdigest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sprint_3_lexdigest;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // É esta linha abaixo que conecta o Java com o seu Layout visual!
+        setContentView(R.layout.activity_main);
+    }
+}
